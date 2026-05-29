18d7ba6b84787d71ad2a383862adbf6a0e7f52d7
amirrezaabdolahi <zerobit0001@gmail.com>
2026-05-29 14:08:35 +0330

    fixing the account problem its not for project

--- frontend/src/utils/env/env.ts
+++ frontend/src/utils/env/env.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 const envSchema = z.object({
   API_TOKEN: z.string().nonempty("API_TOKEN is required"),
+  API_BASE_URL: z.string().nonempty("API_BASE_URL is required"),
 });
 
 export const env = envSchema.parse(process.env);
