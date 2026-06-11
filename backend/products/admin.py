e2b4b7f1fefdd0d224f2dd2024a7d786ece9e126
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-11 12:58:12 +0330

    Add product admin panel

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'shop', 'category', 'buy_price', 'sell_price', 'stock', 'created_at']
+    list_filter = ['shop', 'category']
+    search_fields = ['name', 'barcode']
+    readonly_fields = ['created_at', 'updated_at']
