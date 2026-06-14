b6e4d46c6dc8b08dd6a8a6042f4ade7966318b49
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-14 22:09:58 +0330

    Add admin panel to debt app.

--- backend/debts/admin.py
+++ backend/debts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Debt
 
-# Register your models here.
+
+@admin.register(Debt)
+class DebtAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_customer', 'shop', 'amount', 'paid_amount', 'get_remaining', 'is_paid', 'created_at']
+    list_filter = ['shop', 'created_at']
+    search_fields = ['customer__customer__full_name', 'customer__customer__phone_number']
+    readonly_fields = ['created_at']
+
+    def get_customer(self, obj):
+        return obj.customer.customer.full_name
+    get_customer.short_description = 'Customer'
+
+    def get_remaining(self, obj):
+        return obj.remaining
+    get_remaining.short_description = 'Remaining'
