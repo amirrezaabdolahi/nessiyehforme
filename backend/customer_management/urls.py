1d963e347bdfaad9b6555395c50c1d7c4d906dec
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-22 11:46:44 +0330

    Add customer verify url code.

--- backend/customer_management/urls.py
+++ backend/customer_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomerListCreateView, CustomerDeleteView, CustomerHistoryView
+from .views import CustomerListCreateView, CustomerDeleteView, CustomerHistoryView, CustomerVerifyView
 
 urlpatterns = [
     path('customers/', CustomerListCreateView.as_view()),
+    path('customers/verify/', CustomerVerifyView.as_view()),
     path('customers/<int:pk>/', CustomerDeleteView.as_view()),
     path('customers/<int:pk>/history/', CustomerHistoryView.as_view())
 ]
