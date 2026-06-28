5fb39f9c6cde850e3ab7d7ded581d39f9a51fdd5
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-06-28 21:57:41 +0330

    static root for setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('DB_HOST', 'postgres.railway.internal'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
